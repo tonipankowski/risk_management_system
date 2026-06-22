c37034c42f7f4f502db5f9f4e8c1d61f6a530e54
tonipankowski <pankowski.antoni@gmail.com>
2026-06-22 23:13:46 +0200

    Add CSVProvider, interchangable data source

--- src/providers.py
+++ src/providers.py
@@ -13,9 +13,25 @@ class YFinanceProvider(DataProvider):
         raw = yf.download(tickers, start=start, end=end, auto_adjust=True)
         mod = (raw["Close"].reset_index().melt(id_vars="Date", var_name="ticker", value_name="adj_close").rename(columns={"Date": "date"}))
         return mod
+
+class CSVProvider(DataProvider):
+    def __init__(self, path):
+        self.path = path
+        
+    def get_prices(self, tickers, start, end):
+        raw = pd.read_csv(self.path, parse_dates=["date"])
+        return raw
                
 if __name__ == "__main__":
-    provider = YFinanceProvider()
-    df = provider.get_prices(["AAPL", "MSFT", "SPY"], "2024-01-01", "2025-01-01")
+    yf_provider = YFinanceProvider()
+    df = yf_provider.get_prices(["AAPL", "MSFT", "SPY"], "2024-01-01", "2025-01-01")
+    df.to_csv("data/sample_prices.csv", index=False)
+    print("YFINANCE:")
     print(df.head())
     print(df.shape)
+    
+    csv_provider = CSVProvider("data/sample_prices.csv")
+    df2 = csv_provider.get_prices(["AAPL", "MSFT", "SPY"], "2024-01-01", "2025-01-01")
+    print("\nCSV:")
+    print(df2.head())
+    print(df2.shape)
