f076d8e457943edbb03262be4d43e2fc0a7fb66c
tonipankowski <pankowski.antoni@gmail.com>
2026-06-23 20:38:55 +0200

    Add AlpacaProvider: API with normalized output, it matches with other Providers

--- src/providers.py
+++ src/providers.py
@@ -1,6 +1,12 @@
 from abc import ABC, abstractmethod
 import pandas as pd
 import yfinance as yf
+import os
+from dotenv import load_dotenv
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.timeframe import TimeFrame
+from alpaca.data.enums import Adjustment
 
 class DataProvider(ABC):
     @abstractmethod
@@ -21,6 +27,21 @@ class CSVProvider(DataProvider):
     def get_prices(self, tickers, start, end):
         raw = pd.read_csv(self.path, parse_dates=["date"])
         return raw
+    
+class AlpacaProvider(DataProvider):
+    def __init__(self):
+        load_dotenv()
+        api_key = os.environ.get("ALPACA_API_KEY")
+        secret_key = os.environ.get("ALPACA_SECRET_KEY")
+        self.client = StockHistoricalDataClient(api_key, secret_key)
+        
+    def get_prices(self, tickers, start, end):
+        request = StockBarsRequest(symbol_or_symbols=tickers, timeframe=TimeFrame.Day, start=start, end=end, adjustment=Adjustment.ALL,)
+        bars = self.client.get_stock_bars(request)
+        raw = bars.df
+        mod = (raw.reset_index()[["timestamp", "symbol", "close"]].rename(columns={"timestamp": "date", "symbol": "ticker", "close": "adj_close"}))
+        mod["date"] = mod["date"].dt.tz_localize(None).dt.normalize()
+        return mod
                
 if __name__ == "__main__":
     yf_provider = YFinanceProvider()
@@ -35,3 +56,9 @@ if __name__ == "__main__":
     print("\nCSV:")
     print(df2.head())
     print(df2.shape)
+    
+    provider = AlpacaProvider()
+    df = provider.get_prices(["AAPL", "MSFT", "SPY"], "2024-01-01", "2025-01-01")
+    print("\nAlpaca:")
+    print(df.head())
+    print(df.shape)
